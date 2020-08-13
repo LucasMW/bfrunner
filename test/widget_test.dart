63f51a8b701a865db3066b09a482f1a5468a9fed
LucasMW <great-lucas@hotmail.com>
2020-08-13 11:02:31 +0100

    Hello World Test

---
+++ test/widget_test.dart
@@ -0,0 +1,34 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:bf_test/main.dart';
+
+void main() {
+  testWidgets("Hello World", (WidgetTester tester) async {
+    final program =
+        '++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.';
+    await tester.pumpWidget(MyApp());
+    expect(find.text("Program"), findsOneWidget);
+    expect(find.text("Hello World"), findsNothing);
+
+    await tester.pump();
+    await tester.enterText(find.byType(TextField), program);
+    expect(find.text(program), findsOneWidget);
+
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+    await tester.tap(find.byIcon(Icons.play_arrow));
+    await tester.pump();
+
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+    expect(find.text("Hello World!\n"), findsOneWidget);
+  });
+}
